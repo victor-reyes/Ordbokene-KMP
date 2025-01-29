4d02aeb0d9fc73984aa090eb9a595c3d0c46bbf2
Victor Reyes <5187756+victor-reyes@users.noreply.github.com>
2025-01-29 14:31:22 +0100

    feat: add Android ViewModel Compose dependency
    This change adds the `androidx.lifecycle:lifecycle-viewmodel-compose-android` dependency to the `composeApp` module and updates the `libs.versions.toml` file with the dependency and its version.

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -25,6 +25,8 @@ kotlin {
       implementation(compose.components.uiToolingPreview)
       implementation(libs.androidx.lifecycle.viewmodel)
       implementation(libs.androidx.lifecycle.runtime.compose)
+      implementation(libs.androidx.lifecycle.viewmodel.compose.android)
+
       implementation(projects.shared)
     }
   }
