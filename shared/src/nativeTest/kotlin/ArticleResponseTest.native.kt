077db79a8967506de6e55785a2841a975fc40e88
Victor Reyes <5187756+victor-reyes@users.noreply.github.com>
2025-01-30 12:23:39 +0100

    fix: update native resource reading logic
    This change updates the native resource reading logic to correctly parse resource paths and read the corresponding files. It now correctly handles paths without a leading `./` and splits on `.` to extract the file name and extension.

--- shared/src/nativeTest/kotlin/ArticleResponseTest.native.kt
+++ shared/src/nativeTest/kotlin/ArticleResponseTest.native.kt
@@ -5,12 +5,9 @@ import platform.Foundation.stringWithContentsOfFile
 
 @OptIn(ExperimentalForeignApi::class)
 actual fun readResources(resourceName: String): String {
-  // split based on "." and "/". We want to strip the leading ./ and
-  // split the extension
-  val pathParts = resourceName.split("[.|/]".toRegex())
-  // pathParts looks like
-  // [, , test_case_input_one, bin]
-  val path = NSBundle.mainBundle.pathForResource("resources/${pathParts[2]}", pathParts[3])
+
+  val pathParts = resourceName.trimStart('.', '/').split(".")
+  val path = NSBundle.mainBundle.pathForResource("resources/${pathParts[0]}", pathParts[1])
   val data = NSString.stringWithContentsOfFile(path!!) as String
 
   return data
