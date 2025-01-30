78d7d168764bbe9e6da4a80d04cffed7bf350a8c
Victor Reyes <5187756+victor-reyes@users.noreply.github.com>
2025-01-30 11:21:22 +0100

    feat: add AutocompleteResponse model and test
    This change introduces a new data model, `AutocompleteResponse`

---
+++ shared/src/commonTest/kotlin/AutocompleteTest.kt
@@ -0,0 +1,31 @@
+import kotlinx.serialization.json.Json
+import no.ordbokene.model.json.autocomplete.AutocompleteResponse
+import no.ordbokene.model.json.autocomplete.Dictionary
+import no.ordbokene.model.json.autocomplete.Suggestion
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+class AutocompleteTest {
+
+  companion object {
+    const val TEST_RESPONSE =
+      """{"q": "tomte", "cnt": 9, "cmatch": 1, "a": {"exact": [["tomte", 3], ["tomtefelt", 2], ["tomtekall", 2]]}}"""
+
+    val json = Json { ignoreUnknownKeys = false }
+  }
+
+  @Test
+  fun shouldParseAutocompleteResponse() {
+    val parsed = json.decodeFromString<AutocompleteResponse>(TEST_RESPONSE)
+
+    assertEquals("tomte", parsed.query)
+    assertEquals(
+      setOf(
+        Suggestion("tomte", Dictionary.BOTH),
+        Suggestion("tomtefelt", Dictionary.NN),
+        Suggestion("tomtekall", Dictionary.NN),
+      ),
+      parsed.suggestions.exact,
+    )
+  }
+}
