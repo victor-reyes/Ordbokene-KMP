1e9bdf52872d6afe3e97abd337c6e4ae8f5dac7e
Victor Reyes <5187756+victor-reyes@users.noreply.github.com>
2025-01-30 20:44:04 +0100

    feat(android): implement article autocompletion

--- composeApp/src/androidMain/kotlin/no/ordbokene/ui/search/SearchViewModel.kt
+++ composeApp/src/androidMain/kotlin/no/ordbokene/ui/search/SearchViewModel.kt
@@ -6,14 +6,19 @@ import androidx.lifecycle.viewModelScope
 import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.flow.stateIn
+import no.ordbokene.model.ArticleRepositoryImpl
+import no.ordbokene.model.DictionaryApiService
 
 class SearchViewModel(private val savedStateHandle: SavedStateHandle) : ViewModel() {
+
+  val repository = ArticleRepositoryImpl(DictionaryApiService())
 
   val query = savedStateHandle.getStateFlow("query", "")
 
   val articles =
     query
-      .map { query -> listOf("Article 1", "Article 2").map { "$it by $query" } }
+      .map(repository::fetchAutocomplete)
+      .map { with(it.suggestions) { exact + similar + inflection + freeText }.map { it.word } }
       .stateIn(viewModelScope, SharingStarted.Eagerly, emptyList())
 
   fun setQuery(query: String) {
