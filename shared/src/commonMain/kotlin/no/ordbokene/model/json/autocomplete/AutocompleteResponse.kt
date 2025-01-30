8dded177838d310fdc96b09ec42a876d4d654a6a
Victor Reyes <5187756+victor-reyes@users.noreply.github.com>
2025-01-30 20:46:24 +0100

    refactor: rename `match` to `matchCount` in `AutocompleteResponse`
    This change renames the `match` field to `matchCount` in the `AutocompleteResponse` data model. It also removes the `IntAsBooleanSerializer` since it is no longer needed.

--- shared/src/commonMain/kotlin/no/ordbokene/model/json/autocomplete/AutocompleteResponse.kt
+++ shared/src/commonMain/kotlin/no/ordbokene/model/json/autocomplete/AutocompleteResponse.kt
@@ -1,31 +1,12 @@
 package no.ordbokene.model.json.autocomplete
 
-import kotlinx.serialization.KSerializer
 import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
-import kotlinx.serialization.descriptors.PrimitiveKind
-import kotlinx.serialization.descriptors.PrimitiveSerialDescriptor
-import kotlinx.serialization.descriptors.SerialDescriptor
-import kotlinx.serialization.encoding.Decoder
-import kotlinx.serialization.encoding.Encoder
 
 @Serializable
 data class AutocompleteResponse(
   @SerialName("q") val query: String,
   @SerialName("cnt") val count: Int,
-  @Serializable(with = IntAsBooleanSerializer::class) @SerialName("cmatch") val match: Boolean,
+  @SerialName("cmatch") val matchCount: Int,
   @SerialName("a") val suggestions: Suggestions,
 )
-
-object IntAsBooleanSerializer : KSerializer<Boolean> {
-  override val descriptor: SerialDescriptor = PrimitiveSerialDescriptor("IntAsBoolean", PrimitiveKind.INT)
-
-  override fun serialize(encoder: Encoder, value: Boolean) = encoder.encodeInt(if (value) 1 else 0)
-
-  override fun deserialize(decoder: Decoder) =
-    when (decoder.decodeInt()) {
-      1 -> true
-      0 -> false
-      else -> throw IllegalArgumentException("Invalid value for boolean: ${decoder.decodeInt()}")
-    }
-}
