2f9832a61731d741c826ce94a711635408510446
Victor Reyes <5187756+victor-reyes@users.noreply.github.com>
2025-01-30 20:42:46 +0100

    feat: add ArticleRepository and ArticleRepositoryImpl
    This change introduces the `ArticleRepository` interface, defining methods for fetching autocomplete suggestions, searching, and fetching articles. It also includes an implementation, `ArticleRepositoryImpl`, which uses the `DictionaryApiService` but currently has placeholder implementations for search and fetchArticle.

---
+++ shared/src/commonMain/kotlin/no/ordbokene/model/ArticleRepository.kt
@@ -0,0 +1,25 @@
+package no.ordbokene.model
+
+import no.ordbokene.model.json.autocomplete.AutocompleteResponse
+import no.ordbokene.model.json.lookup.ArticleResponse
+import no.ordbokene.model.json.search.SearchResponse
+
+interface ArticleRepository {
+  suspend fun fetchAutocomplete(query: String): AutocompleteResponse
+
+  suspend fun search(query: String): List<SearchResponse>
+
+  suspend fun fetchArticle(id: Int): ArticleResponse
+}
+
+class ArticleRepositoryImpl(val service: DictionaryApiService) : ArticleRepository {
+  override suspend fun fetchAutocomplete(query: String) = service.fetchAutocomplete(query)
+
+  override suspend fun search(query: String): List<SearchResponse> {
+    TODO("Not yet implemented")
+  }
+
+  override suspend fun fetchArticle(id: Int): ArticleResponse {
+    TODO("Not yet implemented")
+  }
+}
