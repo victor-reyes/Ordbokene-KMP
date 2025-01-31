b3bf46e698db8987c8056c0593a048fb24a77fc2
Victor Reyes <5187756+victor-reyes@users.noreply.github.com>
2025-01-31 13:13:00 +0100

    chore: add material3 library

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -20,6 +20,7 @@ kotlin {
       implementation(compose.runtime)
       implementation(compose.foundation)
       implementation(compose.material)
+      implementation(libs.androidx.material3)
       implementation(compose.ui)
       implementation(compose.components.resources)
       implementation(compose.components.uiToolingPreview)
