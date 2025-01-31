ce47b03ac120f77ce61bb6030db3676cc7c58dac
Victor Reyes <5187756+victor-reyes@users.noreply.github.com>
2025-01-31 08:50:52 +0100

    refactor: move autocomplete request to IO dispatcher

--- shared/src/commonMain/kotlin/no/ordbokene/model/ArticleRepository.kt
+++ shared/src/commonMain/kotlin/no/ordbokene/model/ArticleRepository.kt
@@ -1,6 +1,9 @@
 package no.ordbokene.model
 
 import com.rickclephas.kmp.nativecoroutines.NativeCoroutines
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.IO
+import kotlinx.coroutines.withContext
 import no.ordbokene.model.json.autocomplete.AutocompleteResponse
 import no.ordbokene.model.json.lookup.ArticleResponse
 import no.ordbokene.model.json.search.SearchResponse
@@ -14,7 +17,8 @@ interface ArticleRepository {
 }
 
 class ArticleRepositoryImpl(val service: DictionaryApiService) : ArticleRepository {
-  override suspend fun fetchAutocomplete(query: String) = service.fetchAutocomplete(query)
+  override suspend fun fetchAutocomplete(query: String) =
+    withContext(Dispatchers.IO) { service.fetchAutocomplete(query) }
 
   override suspend fun search(query: String): List<SearchResponse> {
     TODO("Not yet implemented")
