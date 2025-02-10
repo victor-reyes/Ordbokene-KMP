592467c10f60ea1c5f696e37c3553df0cf283241
Victor Reyes <5187756+victor-reyes@users.noreply.github.com>
2025-02-10 20:29:52 +0100

    feat(ui): "move in" Autocomplete into TextField

--- composeApp/src/androidMain/kotlin/no/ordbokene/ui/search/AutocompleteSearchField.kt
+++ composeApp/src/androidMain/kotlin/no/ordbokene/ui/search/AutocompleteSearchField.kt
@@ -8,19 +8,25 @@ import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.IntrinsicSize
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.itemsIndexed
+import androidx.compose.foundation.text.BasicTextField
 import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.ExperimentalMaterialApi
+import androidx.compose.material.IconButton
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Clear
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.ElevatedCard
 import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.derivedStateOf
@@ -28,13 +34,12 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.focus.FocusRequester
 import androidx.compose.ui.focus.focusRequester
 import androidx.compose.ui.focus.onFocusChanged
-import androidx.compose.ui.layout.onGloballyPositioned
-import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.platform.LocalFocusManager
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.ImeAction
@@ -53,22 +58,17 @@ fun AutocompleteSearchField(
   val focusManager = LocalFocusManager.current
   val focusRequester = remember { FocusRequester() }
   var showSuggestions by remember { mutableStateOf(false) }
-
-  var textFieldWidthPx by remember { mutableStateOf(0.dp) }
-  val density = LocalDensity.current
-
   val modifier by remember {
-    derivedStateOf { if (showSuggestions) Modifier.Companion.fillMaxWidth() else Modifier.Companion }
+    derivedStateOf { if (showSuggestions) Modifier.fillMaxWidth() else Modifier.width(256.dp) }
   }
-  Column(Modifier.Companion.fillMaxWidth().background(MaterialTheme.colorScheme.surface)) {
-    OutlinedTextField(
+  ElevatedCard(
+    modifier.background(MaterialTheme.colorScheme.surface),
+    elevation = CardDefaults.elevatedCardElevation(defaultElevation = if (showSuggestions) 16.dp else 4.dp),
+  ) {
+    BasicTextField(
       query,
       onQueryChanged,
-      modifier
-        .focusRequester(focusRequester)
-        .onFocusChanged { showSuggestions = it.isFocused }
-        .onGloballyPositioned { coordinates -> textFieldWidthPx = with(density) { coordinates.size.width.toDp() } }
-        .animateContentSize(),
+      modifier.focusRequester(focusRequester).onFocusChanged { showSuggestions = it.isFocused }.animateContentSize(),
       interactionSource = interactionSource,
       keyboardOptions = KeyboardOptions(imeAction = ImeAction.Companion.Search),
       keyboardActions =
@@ -79,25 +79,34 @@ fun AutocompleteSearchField(
           }
         ),
       singleLine = true,
-    )
-    AnimatedVisibility(showSuggestions) {
-      ElevatedCard(Modifier.width(textFieldWidthPx).heightIn(max = 256.dp)) {
-        LazyColumn {
-          itemsIndexed(suggestions) { index, suggestion ->
-            Row(
-              Modifier.Companion.fillParentMaxWidth().clickable {
-                onSearch(suggestion)
-                focusManager.clearFocus()
+    ) {
+      Column {
+        Row(verticalAlignment = Alignment.CenterVertically) {
+          Spacer(Modifier.width(8.dp))
+          it()
+          Spacer(modifier.weight(1f))
+          IconButton(onClick = { focusRequester.requestFocus() }) {
+            Icon(imageVector = Icons.Filled.Clear, contentDescription = "clear")
+          }
+        }
+        AnimatedVisibility(showSuggestions) {
+          LazyColumn(Modifier.heightIn(max = 256.dp)) {
+            itemsIndexed(suggestions) { index, suggestion ->
+              Row(
+                Modifier.Companion.fillParentMaxWidth().clickable {
+                  onSearch(suggestion)
+                  focusManager.clearFocus()
+                }
+              ) {
+                Text(
+                  suggestion,
+                  Modifier.Companion.padding(12.dp),
+                  fontSize = MaterialTheme.typography.labelLarge.fontSize,
+                  fontWeight = FontWeight.Companion.Bold,
+                )
               }
-            ) {
-              Text(
-                suggestion,
-                Modifier.Companion.padding(12.dp),
-                fontSize = MaterialTheme.typography.labelLarge.fontSize,
-                fontWeight = FontWeight.Companion.Bold,
-              )
+              if (index < suggestions.lastIndex) HorizontalDivider(Modifier.Companion.width(IntrinsicSize.Max))
             }
-            if (index < suggestions.lastIndex) HorizontalDivider(Modifier.Companion.width(IntrinsicSize.Max))
           }
         }
       }
