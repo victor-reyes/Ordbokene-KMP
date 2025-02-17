866a4edc0bed6c282b3c8491874802a269fadf86
Victor Reyes <5187756+victor-reyes@users.noreply.github.com>
2025-02-17 09:15:22 +0100

    refactor: make DictionaryApiService to accept HttpClient as a constructor parameter and configure shared HttpClient instance

--- shared/src/commonMain/kotlin/no/ordbokene/di/SharedModule.kt
+++ shared/src/commonMain/kotlin/no/ordbokene/di/SharedModule.kt
@@ -1,8 +1,10 @@
 package no.ordbokene.di
 
+import io.ktor.client.HttpClient
 import no.ordbokene.model.ArticleRepository
 import no.ordbokene.model.ArticleRepositoryImpl
 import no.ordbokene.model.DictionaryApiService
+import no.ordbokene.model.httpClient
 import org.koin.core.module.dsl.bind
 import org.koin.core.module.dsl.singleOf
 import org.koin.dsl.module
@@ -10,4 +12,5 @@ import org.koin.dsl.module
 val sharedModule = module {
   singleOf(::DictionaryApiService)
   singleOf(::ArticleRepositoryImpl) { bind<ArticleRepository>() }
+  single<HttpClient> { httpClient }
 }
